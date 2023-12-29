c7f4efdc9caceeb5a700fd330bf1df78c449be3a
Michael Alvarez <Michael.alv.dev@gmail.com>
2023-12-29 16:01:07 -0400

    Fix render

--- src/apis/fetchGiff.tsx
+++ src/apis/fetchGiff.tsx
@@ -1,14 +1,14 @@
 const API_KEY = "2cZkiFTqyiS79UdSapL6LHWlublpl7iy";
-export async function fetchGif(factCut:string) {
-    
-    const response = await fetch(`http://api.giphy.com/v1/gifs/search?q=${factCut}&api_key=${API_KEY}`);
+export async function fetchGif(factCut: string) {
+
+    const response = await fetch(`https://api.giphy.com/v1/gifs/search?q=${factCut}&api_key=${API_KEY}`);
     if (!response.ok) {
         throw new Error("Failed fetch GIF");
     }
     const mainRes = await response.json();
-  
+
     const gifsRes = mainRes.data.map((gif: any) => ({
-        url:gif.images.downsized_large.url,
+        url: gif.images.downsized_large.url,
     }));
     return gifsRes;
 
